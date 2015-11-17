8e7c0df89a4a3b1b4b4d1f5c5093f4b5bc233965
Gulmira <miramaxeast@yahoo.com>
2015-11-16 21:06:32 -0800

    we like cheese

--- file-exercises/compare_favs.py
+++ file-exercises/compare_favs.py
@@ -5,3 +5,10 @@ print alba
 vicky_favs = open("vicky_fav_foods.txt")
 vicky = vicky_favs.read()
 print vicky 
+
+def compare_favs(alba_favs, vicky_favs):
+	if alba_favs == vicky_favs:
+		print "Our favorite foods are the same!"
+	else:
+		print 'our favorite foods are different!'
+compare_favs(alba_favs, vicky_favs)
