016f1ecd6cd0c68914c3f323ee00ec2d78ea8a8d
Gulmira <miramaxeast@yahoo.com>
2015-11-16 20:51:45 -0800

    comparing lists

--- file-exercises/compare_favs.py
+++ file-exercises/compare_favs.py
@@ -1,9 +1,3 @@
-alba_favs = ["pasta", "steak", "cheese"]
-vicky_favs = ["burritos", "cheese", "waffles"] 
-
-def compare_favs2(abla_favs, vicky_favs): 
-	if alba_favs[0:] or vicky_favs[0:]: 
-		print "we both love cheese!" 
-	else:
-		print "we have different taste in food  "
-compare_favs2(alba_favs, vicky_favs)
+alba_favs = open("alba_fav_foods.txt")
+alba = alba_favs.read()
+print alba 
