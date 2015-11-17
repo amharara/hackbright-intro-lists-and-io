bda76c520801c697ae5a97621d3bec8181888cf5
Gulmira <miramaxeast@yahoo.com>
2015-11-16 20:44:12 -0800

    alba favorite food

---
+++ file-exercises/compare_favs.py
@@ -0,0 +1,9 @@
+alba_favs = ["pasta", "steak", "cheese"]
+vicky_favs = ["burritos", "cheese", "waffles"] 
+
+def compare_favs2(abla_favs, vicky_favs): 
+	if alba_favs[0:] or vicky_favs[0:]: 
+		print "we both love cheese!" 
+	else:
+		print "we have different taste in food  "
+compare_favs2(alba_favs, vicky_favs)
